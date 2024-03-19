75772e49419789d3d3074b9dbb57d66479b689b6
Andrii Chornyi <ixxxx@i.ua>
2024-03-19 15:42:44 +0200

    for Nazar

--- src/script.js
+++ src/script.js
@@ -12,5 +12,118 @@ document.addEventListener('DOMContentLoaded', function() {
     currentImageIndex = (currentImageIndex + 1) % images.length;
   }
 
-  setInterval(changeImageVisibility, 5000);
+  setInterval(changeImageVisibility, 3000);
+});
+
+//coment функція яка відправляє данні форми
+document.getElementById('myForm').addEventListener('submit', async function(event) {
+  event.preventDefault(); // Зупинити стандартну подію подання форми
+
+  // Отримати дані з форми
+  const name = document.getElementById('name').value;
+  const message = document.getElementById('message').value;
+  const phone = document.getElementById('phone').value;
+
+  try {
+    // Виконати POST-запит на сервер і передати дані
+    const response = await fetch('https://send-data-to-telegram.onrender.com/submit-form', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, message, phone }),
+    });
+
+    // Перевірити, чи відповідь успішна
+    if (!response.ok) {
+      throw new Error('Помилка при відправленні даних на сервер');
+    }
+
+    // Вивести повідомлення про успіх
+    alert('Дані успішно відправлено у чат Telegram!');
+
+    // Очистити поля форми
+    this.reset();
+  } catch (error) {
+    // Вивести повідомлення про помилку у разі неуспішної відправки
+    console.error('Сталася помилка:', error);
+    alert('Виникла помилка при відправленні даних');
+  }
+});
+
+// кнопка для соц сетей
+let button;
+let buttonImage;
+let socialIcons;
+let animationInterval = null;
+let currentIndex = 0;
+
+const images = [
+  './Photo/telegram.jpg',
+  './Photo/viber.jpg',
+  './Photo/close.jpg',
+];
+
+function startAnimation() {
+  buttonImage.style.width = '40px'
+  buttonImage.style.height = '40px'
+
+  if (animationInterval === null) {
+    animationInterval = setInterval(function() {
+      currentIndex = (currentIndex + 1) % (images.length - 1);
+      buttonImage.classList.add('scale-out');
+      setTimeout(() => {
+        buttonImage.src = images[currentIndex];
+        buttonImage.classList.remove('scale-out');
+      }, 500);
+    }, 1500);
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  button = document.querySelector('#social-button');
+  buttonImage = document.getElementById('buttonImage');
+  socialIcons = document.querySelector('.social-icons');
+
+  socialIcons.style.display = 'none';
+
+  function stopAnimation() {
+    clearInterval(animationInterval);
+    animationInterval = null;
+  }
+
+  startAnimation();
+
+  button.addEventListener('click', function() {
+    if (currentIndex === (images.length - 1)) {
+      buttonImage.src = images[0];
+      socialIcons.style.display = 'none';
+      socialIcons.classList.remove('show-icons');
+      startAnimation();
+      currentIndex = 0;
+    } else {
+      buttonImage.src = images[images.length - 1];
+      socialIcons.style.display = 'flex';
+      socialIcons.classList.add('show-icons');
+      stopAnimation();
+      currentIndex = images.length - 1;
+
+      buttonImage.style.width = '20px'
+      buttonImage.style.height = '20px'
+    }
+  });
+});
+
+document.addEventListener('click', function(event) {
+  const isClickInsideButton = button.contains(event.target);
+  const isClickInsideSocialIcons = socialIcons.contains(event.target);
+
+  if (!isClickInsideButton && !isClickInsideSocialIcons) {
+    socialIcons.style.display = 'none';
+    socialIcons.classList.remove('show-icons');
+    buttonImage.src = images[0];
+    startAnimation();
+    currentIndex = 0;
+  }
 });
